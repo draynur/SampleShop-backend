33729255b98a65112569db51be2df3503ba2a8f3
John Runyard <jrunyard3ft@gmail.com>
2021-04-27 00:17:44 -0500

    Updated front-end

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.get('*', function (req,res) {
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
